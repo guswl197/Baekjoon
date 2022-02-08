00e0d2c6c92cf0faf1205f589ebef96c17c6c1f6
guswl197 <guswl197@ajou.ac.kr>
2022-02-08 15:17:26 +0900

    바이러스 dfs, bfs

--- Baekjoon/Baekjoon/바이러스.cpp
+++ Baekjoon/Baekjoon/바이러스.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+using namespace std;
+
+int n, cnt = 0;
+int map[101][101];
+int visit[101];
+
+void bfs(int x) {
+	visit[x] = 1;
+	queue<int> q;
+	q.push(x);
+
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+
+		for (int i = 1; i <= n; i++) {
+			if (!visit[i] && map[node][i]) {
+				q.push(i);
+				visit[i] = 1;
+				cnt++;
+			}
+		}
+	}
+}
+
+int main(void) {
+
+	int p;
+	int u, v;
+	cin >> n;
+	cin >> p;
+
+	for (int i = 0; i < p; i++) {
+		cin >> u >> v;
+		map[u][v] = map[v][u] = 1;
+	}
+
+	bfs(1);
+
+	cout << cnt << '\n';
+}
+/*
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int n, cnt = 0;
@@ -33,3 +78,4 @@ int main(void) {
 
 	cout << cnt << '\n';
 }
+*/
