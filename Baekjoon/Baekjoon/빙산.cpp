bbff3ea8f0fc471e5766569e08b69b6c2f5df951
guswl197 <guswl197@ajou.ac.kr>
2022-02-11 10:38:09 +0900

    빙산 dfs+bfs 수정

--- Baekjoon/Baekjoon/빙산.cpp
+++ Baekjoon/Baekjoon/빙산.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <queue>
+#include <cstring>
 
 using namespace std;
 
 int n, m; 
 int ans = 0; 
+int num = 0; 
 int arr[301][301]; 
 int visit[301][301]; 
 int dx[4] = { 1,-1,0,0 }; 
-int dy[4] = { 0,0,1,-1 };
+int dy[4] = { 0,0,1,-1};
 queue<pair<int, int>> q; 
 
 void bfs() {
@@ -31,6 +33,37 @@ void bfs() {
 	}
 }
 
+void check(int x, int y) {
+	visit[x][y] = 1;
+
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (0 > nx || nx >= n || 0 > ny || ny >= m) {
+			continue;
+		}
+
+		if (arr[nx][ny] && visit[nx][ny] == 0) {
+			check(nx, ny); 
+		}
+	}
+}
+
+int block() {
+	num = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!visit[i][j] && arr[i][j]) {
+				check(i, j);
+				num++;
+			}
+		}
+	}
+
+	return num; 
+}
+
 int main(void) {
 	cin >> n >> m; 
 	for (int i = 0; i < n; i++) {
@@ -38,6 +71,13 @@ int main(void) {
 			cin >> arr[i][j];
 		}
 	}
+
+	if (block() >= 2) {
+		cout << 0 << '\n';
+		return 0; 
+	}
+
+	memset(visit, 0, sizeof(visit));
 
 	while (1) {
 		for (int i = 0; i < n; i++) {
@@ -48,15 +88,22 @@ int main(void) {
 				}
 			}
 		}
-		bfs();
-		ans++;
-		if (!q.empty()) {
-			break; 
+
+		if (q.empty()) {
+			cout << 0 << '\n';
+			return 0; 
 		}
+
+		bfs(); 
+		ans++; 
 		q = queue<pair<int, int>>();
 		memset(visit, 0, sizeof(visit)); 
+
+		if (block() >= 2) {
+			cout << ans << '\n';
+			return 0;
+		}
 	}
 
-	cout << ans << '\n'; 
 	return 0; 
 }
