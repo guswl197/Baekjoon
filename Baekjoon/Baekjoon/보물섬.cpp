28edefed7c04ec8e0771a4bf8881c908e7a98497
guswl197 <guswl197@ajou.ac.kr>
2022-02-14 15:36:29 +0900

    보물섬 bfs

--- Baekjoon/Baekjoon/보물섬.cpp
+++ Baekjoon/Baekjoon/보물섬.cpp
@@ -1,63 +1,65 @@
-#include <stdio.h>
 #include <iostream>
-#include <stdlib.h>
-#include <cstring>
 #include <queue>
+#include <cstring>
+#include <algorithm>
+
 using namespace std;
 
-int dy[4] = { 1,-1,0,0 };
-int dx[4] = { 0,0,1,-1 };
-char map[50][50];
-bool visit[50][50];
-int t = 0;
 int n, m;
+char arr[51][51]; 
+int visit[51][51]; 
+int dx[4] = { 1,-1,0,0 }; 
+int dy[4] = { 0,0,1,-1 }; 
 
 void bfs(int x, int y) {
-	int path = 0;
-	memset(visit, 0, sizeof(visit));
-	queue< pair<int, int> > q;
+	queue<pair<int, int>> q;
 	q.push(make_pair(x, y));
-	visit[x][y] = true;
+	visit[x][y] = 1;
 
 	while (!q.empty()) {
-		int qsize = q.size();
+		int cur_x = q.front().first;
+		int cur_y = q.front().second;
+		q.pop();
 
-		for (int i = 0; i < qsize; ++i) {
-			int cur_x = q.front().first;
-			int cur_y = q.front().second;
-			q.pop();
-
-			for (int j = 0; j < 4; ++j) {
-				int nx = cur_x + dx[j];
-				int ny = cur_y + dy[j];
-
-				if (0 > nx || nx >= n || 0 > ny || ny >= m) continue;
-				if (map[nx][ny] == 'W' || visit[nx][ny]) continue;
-
+		for (int i = 0; i < 4; i++) {
+			int nx = cur_x + dx[i];
+			int ny = cur_y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue;
+			}
+			if (arr[nx][ny] == 'L' && !visit[nx][ny]) {
 				q.push(make_pair(nx, ny));
-				visit[nx][ny] = true;
+				visit[nx][ny] = visit[cur_x][cur_y] + 1;
 			}
 		}
-		path++;
 	}
-	t = max(t, path - 1);
 }
 
-int main() {
-	cin >> n >> m;
+int main(void){
+	cin >> n >> m; 
+	vector<int> v; 
 
-	for (int i = 0; i < n; ++i) {
-		cin >> map[i];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (map[i][j] == 'L') {
+			if (arr[i][j] == 'L') {
+				memset(visit, 0, sizeof(visit)); 
 				bfs(i, j);
+				int num = 0;
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < m; j++) {
+						num = max(num, visit[i][j]);
+					}
+				}
+				v.push_back(num);
 			}
+
 		}
 	}
 
-	cout << t;
-
+	cout <<*max_element(v.begin(), v.end())-1<<'\n'; 
+	return 0; 
 }
