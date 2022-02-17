4c5dcf2c5a5cc34bdb55d670a870df5f3d075736
guswl197 <guswl197@ajou.ac.kr>
2022-02-17 14:08:47 +0900

    그리디

---
+++ Baekjoon/Baekjoon/신입 사원.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(void) {
+	int t; 
+	cin >> t; 
+
+	for (int i = 0; i < t; i++) {
+		int n; 
+		int min = 100001; 
+		cin >> n; 
+		int ans = 0;
+		vector<pair<int, int>> v; 
+
+		for (int i = 0; i < n; i++) {
+			int a, b; 
+			cin >> a >> b; 
+			v.push_back(make_pair(a, b)); 
+		}
+
+		sort(v.begin(), v.end()); 
+
+		for (int i = 0; i < n; i++) {
+			if (min > v[i].second) {
+				min = v[i].second;
+				ans++; 
+			}
+		}
+
+		cout << ans << '\n'; 
+	}
+
+	return 0; 
+}
