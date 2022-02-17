da095c6a3fc56c84bd4cdf415025b11de2fda705
guswl197 <guswl197@ajou.ac.kr>
2022-02-17 10:34:35 +0900

    잃어버린 괄호 그리디

--- Baekjoon/Baekjoon/잃어버린 괄호.cpp
+++ Baekjoon/Baekjoon/잃어버린 괄호.cpp
@@ -11,7 +11,7 @@ int main(void) {
 	int result = 0;
 	bool minus = false;
 
-	for (int i = 0; i < str.size(); i++) {
+	for (int i = 0; i <= str.size(); i++) {
 		if (str[i] == '+' || str[i] == '-' || str[i] == '\0') {
 			if (minus == true) {
 				result += -stoi(temp); 
@@ -22,11 +22,10 @@ int main(void) {
 			temp = "";
 			if (str[i] == '-') {
 				minus = true;
-				continue;
 			}
-			temp += str[i]; 
+			continue;
 		}
-		
+		temp += str[i];
 	}
 	cout << result << '\n'; 
 	return 0; 
