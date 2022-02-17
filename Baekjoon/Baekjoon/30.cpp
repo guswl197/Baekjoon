1e2d548cf5cebe1944ba08cdbe4dc6cb0bd58f6e
guswl197 <guswl197@ajou.ac.kr>
2022-02-17 13:06:38 +0900

    30 그리디

---
+++ Baekjoon/Baekjoon/30.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int number[10];
+
+int main(void) {
+	string a; 
+	cin >> a;
+	int temp = 0; 
+	for (int i = 0; i < a.size(); i++) {
+		number[a[i] - '0']++;
+		temp += a[i] - '0'; 
+	}
+	if (temp % 3 == 0 && number[0] != 0) {
+		for (int i = 9; i >= 0; i--) {
+			for (int j = 0; j < number[i]; j++) {
+				cout << i; 
+			}
+		}
+	}
+	else {
+		cout << -1;
+	}
+
+	cout << '\n'; 
+	return 0; 
+}
