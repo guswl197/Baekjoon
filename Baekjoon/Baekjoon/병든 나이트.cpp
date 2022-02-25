0f9b326066fcd3427ce58b06b06588129ff560a2
guswl197 <guswl197@ajou.ac.kr>
2022-02-25 14:23:25 +0900

    그리디

--- Baekjoon/Baekjoon/병든 나이트.cpp
+++ Baekjoon/Baekjoon/병든 나이트.cpp
@@ -3,22 +3,25 @@
 
 using namespace std;
 
-int n, m;
-
 int main(void) {
+	int n, m;
 	cin >> n >> m; 
+
 	if (n == 1) {
-		cout << 1; 
+		cout << 1 << '\n'; 
 	}
 	else if (n == 2) {
-		cout << min(4, (m + 1) / 2)<<'\n'; 
+		cout << min(4, (m + 1) / 2) << '\n';
 	}
 	else {
-		if (m <= 6) {
-			cout << min(4, m) << '\n';
+		if (m <= 4) {
+			cout << m << '\n';
+		}
+		else if (m == 5) {
+			cout << 4 << '\n';
 		}
 		else {
-			cout << m - 2 << '\n';
+			cout << m-2 << '\n';
 		}
 	}
 
