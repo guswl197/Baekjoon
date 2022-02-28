f65602aaeb90e172a604e4dba16946f273c76a87
guswl197 <guswl197@ajou.ac.kr>
2022-02-28 11:17:56 +0900

    그리디

---
+++ Baekjoon/Baekjoon/문서 검색.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	string str;
+	string word; 
+	getline(cin,str);
+
+	getline(cin, word);
+	int ans = 0;
+	
+	while (1) {
+		if (str.find(word) != string::npos) {
+			ans++;
+			int idx = str.find(word); 
+			str.erase(0, word.size()+idx); 
+		}
+		else {
+			break; 
+		}
+	}
+
+	cout << ans << '\n'; 
+	return 0; 
+}
