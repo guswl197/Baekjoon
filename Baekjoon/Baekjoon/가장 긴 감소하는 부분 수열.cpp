5d37fca4f51213308afb39ec5bf741c50fe0deb7
guswl197 <guswl197@ajou.ac.kr>
2022-03-02 15:21:50 +0900

    dp

--- Baekjoon/Baekjoon/가장 긴 감소하는 부분 수열.cpp
+++ Baekjoon/Baekjoon/가장 긴 감소하는 부분 수열.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,21 +8,25 @@ int d[10001];
 
 int main(void) {
 	int n;
+	int result = 0; 
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
 
-	d[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		if (a[i - 1] > a[i]) {
-			d[i] = d[i - 1] + 1;
-		}
-		else {
-			d[i] = d[i - 1]; 
+	for (int i = 1; i <= n; i++) {
+		d[i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (a[i] < a[j] && d[i] < d[j] + 1) {
+				d[i] = d[j] + 1; 
+			}
 		}
 	}
 
-	cout << d[n] << '\n';
+	for (int i = 1; i <= n; i++) {
+		result = max(result, d[i]); 
+	}
+
+	cout << result << '\n';
 	return 0; 
 }
