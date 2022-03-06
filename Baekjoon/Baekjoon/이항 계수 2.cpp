08df39755ee057277b7e1a56dc2338e8ffb3b917
guswl197 <guswl197@ajou.ac.kr>
2022-03-06 18:51:07 +0900

    dp

--- Baekjoon/Baekjoon/이항 계수 2.cpp
+++ Baekjoon/Baekjoon/이항 계수 2.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-long long d[1001][1001];
+int d[1001][1001]; 
 
 int main(void) {
 	int n, k;
-	long long ans = 0;
-	cin >> n >> k;
+	cin >> n >> k; 
 
-	d[1][0] = 1;
 	d[1][1] = 1;
+	d[1][0] = 1;
 
 	for (int i = 2; i <= n; i++) {
 		for (int j = 0; j <= k; j++) {
-			d[i][j] = d[i - 1][j - 1] % 10007 + d[i - 1][j] % 10007; 
+			d[i][j] = d[i - 1][j - 1] % 10007 + d[i - 1][j] % 10007;
 		}
 	}
 
-	cout << d[n][k]%10007 << '\n';
+	cout << d[n][k] % 10007 << '\n';
 	return 0; 
 }
