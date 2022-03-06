619b6ad4b4768944b774433e4010e70982f55200
guswl197 <guswl197@ajou.ac.kr>
2022-03-06 19:24:36 +0900

    dp

--- Baekjoon/Baekjoon/1, 2, 3 더하기 5.cpp
+++ Baekjoon/Baekjoon/1, 2, 3 더하기 5.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
-
-const long long mod = 1000000009;
+#include <vector>
+#include<algorithm>
 
 using namespace std;
 
+vector<int> v; 
 long long d[100001][4];
 
-int main()
-{
-	for (int i = 1; i <= 100000; i++) {
-		if (i >= 1) {
-			d[i][1] = d[i - 1][2] + d[i - 1][3];
-			if (i == 1) {
-				d[i][1] = 1;
-			}
-		}
-		if (i >= 2) {
-			d[i][2] = d[i - 2][1] + d[i - 2][3];
-			if (i == 2) {
-				d[i][2] = 1;
-			}
-		}
-		if (i >= 3) {
-			d[i][3] = d[i - 3][1] + d[i - 3][2];
-			if (i == 3) {
-				d[i][3] = 1;
-			}
-		}
-		d[i][1] %= mod;
-		d[i][2] %= mod;
-		d[i][3] %= mod;
-	}
-
-	int t;
+int main(void) {
+	int t; 
 	cin >> t;
-	while (t--) {
-		int n;
-		cin >> n;
-		cout << (d[n][1] + d[n][2] + d[n][3]) % mod << '\n';
+	v.push_back(0); 
+	for (int i = 0; i < t; i++) {
+		int a; 
+		cin >> a;
+		v.push_back(a); 
 	}
 
-	return 0;
+	int ma = *max_element(v.begin(), v.end()); 
+
+	d[1][1] = 1;
+	d[2][2] = 1;
+	d[3][3] = 1;
+	d[3][2] = 1;
+	d[3][1] = 1;
+	for (int i = 4; i <= ma; i++) {
+		if (i - 3 >= 0) {
+			d[i][3] = d[i - 3][2]% 1000000009 + d[i - 3][1] % 1000000009;
+		}
+		if (i - 2 >= 0) {
+			d[i][2] = d[i - 2][3] % 1000000009 + d[i - 2][1] % 1000000009 ;
+		}
+		if (i - 1 >= 0) {
+			d[i][1] = d[i - 1][2] % 1000000009 + d[i - 1][3] % 1000000009;
+		}
+	}
+
+	for (int i = 1; i <= t; i++) {
+		long long ans =d[v[i]][1] + d[v[i]][2] + d[v[i]][3];
+		cout << ans % 1000000009 << '\n';
+	}
+	return 0; 
 }
