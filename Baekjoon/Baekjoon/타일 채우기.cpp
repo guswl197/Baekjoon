c3086d1404b48783be4e6e95f95eb1a2ebd79f50
guswl197 <guswl197@ajou.ac.kr>
2022-03-08 15:03:01 +0900

    dp

---
+++ Baekjoon/Baekjoon/타일 채우기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+long long d[31];
+
+int main(void) {
+	int n;
+	cin >> n;
+	d[0] = 1;
+
+	for (int i = 2; i <= n; i += 2) {
+		d[i] = 3 * d[i - 2];
+		for (int j = i - 4; j >= 0; j -= 2) {
+			d[i] += 2 * d[j];
+		}
+	}
+
+	cout << d[n] << '\n';
+	return 0;
+}
