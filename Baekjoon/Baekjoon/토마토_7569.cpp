f0f48bfdc20625649bd2891fe9d7592305e4375f
guswl197 <guswl197@ajou.ac.kr>
2022-03-10 15:06:57 +0900

    bfs

--- Baekjoon/Baekjoon/토마토_7569.cpp
+++ Baekjoon/Baekjoon/토마토_7569.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
-#include <queue>
+#include<queue>
 #include <tuple>
-#include <algorithm>
 
 using namespace std;
 
-int n, m, h; 
-int ans = 0;
-int arr[101][101][101]; 
-int visit[101][101][101]; 
-int dx[6] = { 1,-1,0,0,0,0 };
-int dy[6] = { 0,0,1,-1,0,0 };
-int dz[6] = { 0,0,0,0,1,-1 };
+int n, m, h;
+int map[101][101][101];
 queue<tuple<int, int, int>> q; 
+int dx[6] = { 1,-1,0,0,0,0 }; 
+int dy[6] = { 0,0,1,-1,0,0 }; 
+int dz[6] = { 0,0,0,0,1,-1 }; 
 
 void bfs() {
 	while (!q.empty()) {
-		int x = get<1>(q.front());
-		int y = get<2>(q.front());
-		int z = get<0>(q.front());
+		int cur_z = get<0>(q.front()); 
+		int cur_x= get<1>(q.front());
+		int cur_y= get<2>(q.front());
 		q.pop(); 
 
 		for (int i = 0; i < 6; i++) {
-			int nx = x + dx[i]; 
-			int ny = y + dy[i];
-			int nz = z + dz[i];
+			int nx = cur_x + dx[i]; 
+			int ny = cur_y + dy[i];
+			int nz = cur_z + dz[i];
 
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) {
 				continue;
 			}
-			if (arr[nz][nx][ny]==0 && !visit[nz][nx][ny]) {
-				visit[nz][nx][ny] = 1; 
-				arr[nz][nx][ny] = arr[z][x][y] + 1; 
+			if (map[nz][nx][ny] == 0) {
+				map[nz][nx][ny] = map[cur_z][cur_x][cur_y] + 1;
 				q.push(make_tuple(nz, nx, ny)); 
 			}
 		}
@@ -39,33 +35,34 @@ void bfs() {
 }
 
 int main(void) {
+	int ans = 0;
 	cin >> m >> n >> h;
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n ; j++) {
-			for (int k = 0; k < m; k++) {
-				cin >> arr[i][j][k]; 
-				if (arr[i][j][k] == 1) {
-					q.push(make_tuple(i, j, k)); 
-				}
-			}
-		}
-	}
-
-	bfs(); 
 
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < m; k++) {
-				if (arr[i][j][k] == 0) {
-					cout << -1 << '\n';
-					return 0; 
+				cin >> map[i][j][k];
+				if (map[i][j][k] == 1) {
+					q.push(make_tuple(i, j, k));
 				}
-				ans = max(ans, arr[i][j][k]);
 			}
 		}
 	}
 
-	cout << ans-1 << '\n';
-	return 0; 
+	bfs();
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				if (map[i][j][k] == 0) {
+					cout << -1 << '\n';
+					return 0;
+				}
+				ans = max(ans, map[i][j][k]);
+			}
+		}
+	}
+
+	cout << ans - 1 << '\n';
+	return 0;
 }
