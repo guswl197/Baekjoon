4bcf8fde320c762badea36a4473ac85a3240aab7
guswl197 <guswl197@ajou.ac.kr>
2022-03-10 20:36:51 +0900

    bfs, dfs

--- Baekjoon/Baekjoon/빙산.cpp
+++ Baekjoon/Baekjoon/빙산.cpp
@@ -6,103 +6,107 @@ using namespace std;
 
 int n, m;
 int ans = 0;
-int num = 0;
 int arr[301][301];
-int visit[301][301];
+int bvisit[301][301];
+int dvisit[301][301];
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 queue<pair<int, int>> q;
 
 void bfs() {
 	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
+		int cur_x = q.front().first;
+		int cur_y = q.front().second;
+		q.pop(); 
 
 		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+			int nx = cur_x + dx[i]; 
+			int ny = cur_y + dy[i];
 
-			if (0 > nx || nx >= n || 0 > ny || ny >= m) {
-				continue;
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue; 
 			}
-			if (arr[nx][ny] == 0 && arr[x][y] > 0 && visit[nx][ny] == 0) {
-				arr[x][y]--;
+			if (!arr[nx][ny] && !bvisit[nx][ny] && arr[cur_x][cur_y]>0) {
+				arr[cur_x][cur_y]--;
 			}
 		}
 	}
 }
 
-void check(int x, int y) {
-	visit[x][y] = 1;
-
+void dfs(int x,int y) {
+	dvisit[x][y] = 1;
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
-		int ny = y + dy[i];
+		int ny = y + dy[i]; 
 
-		if (0 > nx || nx >= n || 0 > ny || ny >= m) {
+		if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
 			continue;
 		}
-
-		if (arr[nx][ny] && visit[nx][ny] == 0) {
-			check(nx, ny);
+		if (arr[nx][ny] && !dvisit[nx][ny]) {
+			dfs(nx, ny); 
 		}
 	}
 }
 
-int block() {
-	num = 0;
+int goDfs() {
+	memset(dvisit, 0, sizeof(dvisit));
+	int cnt = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (!visit[i][j] && arr[i][j]) {
-				check(i, j);
-				num++;
+			if (arr[i][j] && !dvisit[i][j]) {
+				dfs(i, j);
+				cnt++;
 			}
 		}
 	}
 
-	return num;
+	return cnt; 
+}
+
+int goBfs() {
+	memset(bvisit, 0, sizeof(bvisit));
+	ans++;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] > 0) {
+				q.push(make_pair(i, j));
+				bvisit[i][j] = 1;
+			}
+		}
+	}
+
+	if (q.empty()) {
+		return -1;
+	}
+
+	bfs(); 
+	return 0;
 }
 
 int main(void) {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+			cin >> arr[i][j]; 
 		}
 	}
 
-	if (block() >= 2) {
+	//처음부터 두 덩어리 이상 
+	if (goDfs() >= 2) {
 		cout << 0 << '\n';
-		return 0;
+		return 0; 
 	}
-
-	memset(visit, 0, sizeof(visit));
 
 	while (1) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (arr[i][j] > 0) {
-					q.push(make_pair(i, j));
-					visit[i][j] = 1;
-				}
-			}
-		}
-
-		if (q.empty()) {
+		if (goBfs() == -1) {
 			cout << 0 << '\n';
 			return 0;
 		}
-
-		bfs();
-		ans++;
-		memset(visit, 0, sizeof(visit));
-
-		if (block() >= 2) {
+		if (goDfs() >= 2) {
 			cout << ans << '\n';
 			return 0;
 		}
 	}
 
-	return 0;
+	return 0; 
 }
