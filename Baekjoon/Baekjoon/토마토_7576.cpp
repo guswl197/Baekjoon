073b5ecc892bf3eb1ab7a94ee4b93ce3eee2406b
guswl197 <guswl197@ajou.ac.kr>
2022-03-10 14:34:13 +0900

    bfs

--- Baekjoon/Baekjoon/토마토_7576.cpp
+++ Baekjoon/Baekjoon/토마토_7576.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-#include  <vector>
-#include <algorithm>
+#include <stdlib.h>
 #include <queue>
 
 using namespace std;
 
-int ans = 0; 
-int n, m; 
-int arr[1001][1001]; 
-int visit[1001][1001]; 
+int m, n;
 queue<pair<int, int>> q;
-int dx[4] = { 0,0,1,-1 }; 
-int dy[4] = { 1,-1,0,0 };
+int map[1001][1001];
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
 
 void bfs() {
-	
-	while(!q.empty()){
+
+	while (!q.empty()) {
 		int cur_x = q.front().first;
-		int cur_y = q.front().second; 
-		q.pop(); 
+		int cur_y = q.front().second;
+		q.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int nx = cur_x + dx[i];
@@ -27,41 +25,40 @@ void bfs() {
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
 				continue;
 			}
-			if (arr[nx][ny] == 0) {
-				arr[nx][ny] = arr[cur_x][cur_y] + 1;
-				q.push(make_pair(nx, ny)); 
+			if (map[nx][ny] == 0) {
+				map[nx][ny] = map[cur_x][cur_y] + 1;
+				q.push(make_pair(nx, ny));
 			}
 		}
 	}
 }
 
 int main(void) {
-	cin >> m >> n; 
-
+	cin >> m >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j]; 
-			if (arr[i][j] == 1) {
-				q.push(make_pair(i, j)); 
+			cin >> map[i][j];
+			if (map[i][j] == 1) {
+				q.push(make_pair(i, j));
 			}
 		}
 	}
+	bfs();
 
-	bfs(); 
-
+	int max = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (arr[i][j] ==0) {
-				cout << -1 << '\n'; 
-				return 0; 
+			if (map[i][j] == 0) {
+				cout << -1 << '\n';
+				return 0;
 			}
 
-			if (ans < arr[i][j]) {
-				ans = arr[i][j]; 
+			if (max < map[i][j]) {
+				max = map[i][j];
 			}
 		}
 	}
-	
-	cout << ans-1 << '\n'; 
+	cout << max - 1 << '\n';
 
+	return 0;
 }
