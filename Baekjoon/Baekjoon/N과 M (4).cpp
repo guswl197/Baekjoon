aa63df47d4d190599a56b914854703ed5d6046cc
guswl197 <guswl197@ajou.ac.kr>
2022-03-14 15:31:24 +0900

    삼성 역량 dfs 백트래킹

---
+++ Baekjoon/Baekjoon/N과 M (4).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10]; 
+
+void dfs(int pos, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n'; 
+	}
+	else {
+		for (int i = pos; i <= n; i++) {
+			arr[cnt] = i; 
+			dfs(i, cnt + 1); 
+		}
+	}
+}
+int main(void) {
+	cin >> n >> m; 
+	dfs(1, 0); 
+	return 0; 
+}
