f5e4cbcd7d87924f1f897601864222f8b7e00416
guswl197 <guswl197@ajou.ac.kr>
2022-03-21 22:33:12 +0900

    구현, 문자열

--- Baekjoon/Baekjoon/개미.cpp
+++ Baekjoon/Baekjoon/개미.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 int main(void) {
 	int n1, n2;
-	int t; 
-	cin >> n1 >> n2; 
+	int t;
+	cin >> n1 >> n2;
 
-	string right, left; 
+	string right, left;
 	cin >> right;
 	cin >> left;
-	cin >> t; 
-	reverse(right.begin(), right.end()); 
-	string ant = right + left; 
+	cin >> t;
+	reverse(right.begin(), right.end());
+	string ant = right + left;
 
 	while (t--) {
 		for (int i = 0; i < n1 + n2 - 1; i++) {
 			if (right.find(ant[i]) != string::npos && left.find(ant[i + 1]) != string::npos) {
-				swap(ant[i], ant[i + 1]); 
-				i++; 
+				swap(ant[i], ant[i + 1]);
+				i++;
 			}
 		}
 	}
 
-	cout << ant << '\n'; 
-	return 0; 
+	cout << ant << '\n';
+	return 0;
 }
