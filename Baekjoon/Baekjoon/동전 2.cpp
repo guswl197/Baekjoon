7be1181f93e09d970126bb0626c2b56bbfdd75b8
guswl197 <guswl197@ajou.ac.kr>
2022-03-23 16:25:10 +0900

    dp

--- Baekjoon/Baekjoon/동전 2.cpp
+++ Baekjoon/Baekjoon/동전 2.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
-
 int coin[101];
-int dp[100001];
+int d[100001];
 
 int main() {
 	int n, k;
 	cin >> n >> k;
-	for (int i = 1; i <= n; i++) cin >> coin[i];
-	fill(dp, dp + 100001, 100001);
-
-	dp[0] = 0;
 	for (int i = 1; i <= n; i++) {
-		for (int j = coin[i]; j <= k; j++)
-			dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+		cin >> coin[i];
 	}
 
-	dp[k] = (dp[k] == 100001) ? -1 : dp[k];
-	cout << dp[k] << endl;
-	return 0;
+	fill(d, d + 100001, 100001); 
+
+	d[0] = 0; 
+	for (int i = 1; i <= n; i++) {
+		for (int j = coin[i]; j <= k; j++) {
+			d[j] = min(d[j], d[j - coin[i]] + 1);
+		}
+	}
+
+	d[k] = d[k] == 100001 ? -1 : d[k]; 
+	cout << d[k]<<'\n';
+	return 0; 
 }
