f03e766b28335b81745232cf4083e7185bfe3904
guswl197 <guswl197@ajou.ac.kr>
2022-03-23 14:51:41 +0900

    dp

--- Baekjoon/Baekjoon/계단 오르기.cpp
+++ Baekjoon/Baekjoon/계단 오르기.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
-int a[305];
-int d[305][3];
+
+int d[305][3]; 
 
 int main(void) {
-
 	int n;
-	cin >> n;
+	cin >> n; 
+	vector<int> v(n+1); 
+
 	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
+		cin >> v[i]; 
 	}
 
-	d[1][1] = a[1];
+	d[1][1] = v[1];
 
 	for (int i = 2; i <= n; i++) {
-
-		d[i][2] = d[i - 1][1] + a[i];
-		d[i][1] = max(d[i - 2][2], d[i - 2][1]) + a[i];
+		d[i][1] = max(d[i - 2][1], d[i - 2][2]) + v[i]; 
+		d[i][2] = d[i - 1][1] + v[i]; 
 	}
 
-	cout << max(d[n][1], d[n][2]) << '\n';
-	return 0;
+	cout << max(d[n][1],d[n][2]) << '\n'; 
+	return 0; 
 }
