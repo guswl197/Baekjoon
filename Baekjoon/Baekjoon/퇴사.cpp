bd0c568391bece0ffac6821207ab0a6a6497f9c4
guswl197 <guswl197@ajou.ac.kr>
2022-03-23 14:09:49 +0900

    dp

---
+++ Baekjoon/Baekjoon/퇴사.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int d[1500000];
+vector<pair<int, int>> v;
+
+int main(void) {
+	int n;
+	cin >> n;
+	v.push_back(make_pair(0, 0));
+	for (int i = 1; i <= n; i++) {
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
+	}
+
+	for (int i = 1; i <= n; i++) {
+		d[i + v[i].first] = max(d[i + v[i].first], d[i] + v[i].second);
+		d[i+1] = max(d[i], d[i + 1]);
+	}
+
+	cout << d[n + 1] << '\n';
+	return 0; 
+}
