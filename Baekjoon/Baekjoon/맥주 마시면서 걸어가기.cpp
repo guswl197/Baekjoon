b3e86a6bc9ce43116441408f8477f8eb0c84dfde
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-06-28 17:04:37 +0900

    bfs

--- Baekjoon/Baekjoon/맥주 마시면서 걸어가기.cpp
+++ Baekjoon/Baekjoon/맥주 마시면서 걸어가기.cpp
@@ -1,13 +1,63 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
-int main(void) {
-	int t; 
-	cin >> t; 
+int n; 
+pair<int, int> sang;
+pair<int, int> goal;
+vector<pair<int, int>> con;
+int visit[101]; 
 
-	for (int i = 0; i < t; i++) {
-		int n; 
-		cin >> n; 
+bool bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	q.push({ x,y }); 
+	
+	while (!q.empty()) {
+		int nx = q.front().first; 
+		int ny = q.front().second; 
+		q.pop(); 
+
+		if (abs(goal.first - nx) + abs(goal.second - ny) <= 1000) {
+			return true;
+		}
+		for (int i = 0; i < con.size(); i++) {
+			if (abs(con[i].first - nx) + abs(con[i].second - ny) <= 1000 && visit[i] == 0) {
+				q.push({ con[i].first, con[i].second });
+				visit[i] = 1;
+			}
+		}
 	}
+
+	return false;
+}
+
+int main() {
+	int t;
+	cin >> t; 
+	while (t--) {
+		cin >> n; 
+		cin >> sang.first >> sang.second; 
+		vector<pair<int, int>>().swap(con);
+		memset(visit, 0, sizeof(visit)); 
+
+		for (int i = 0; i < n; i++) {
+			int a, b;
+			cin >> a >> b;
+			con.push_back({ a,b }); 
+		}
+
+		cin >> goal.first >> goal.second; 
+
+		if (bfs(sang.first, sang.second)==true) {
+			cout << "happy" << '\n';
+		}
+		else {
+			cout << "sad" << '\n';
+		}
+	}
+
+	return 0;
 }
