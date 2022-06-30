10d0486be6542b088f56c58f5d35a784df465a54
guswl197 <guswl197@ajou.ac.kr>
2022-06-30 17:37:00 +0900

    백트래킹

---
+++ Baekjoon/Baekjoon/부분수열의 합.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n, s;
+int arr[30];
+int cnt = 0;
+
+void func(int cur, int tot) {
+	if (cur == n) {
+		if (tot == s) {
+			cnt++;
+		}
+		return;
+	}
+	func(cur + 1, tot); //선택X
+	func(cur + 1, tot + arr[cur]); //선택O
+}
+int main(void) {
+	cin >> n >> s;
+	for (int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+
+	func(0, 0);
+	if (s == 0) {
+		cnt--;
+	}
+	cout << cnt<<'\n';
+	return 0; 
+}
