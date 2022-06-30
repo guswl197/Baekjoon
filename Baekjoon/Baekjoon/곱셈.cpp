2c229f1240a7320da2f1dd3234fe572ddd11ac4e
guswl197 <guswl197@ajou.ac.kr>
2022-06-30 10:31:30 +0900

    재귀

---
+++ Baekjoon/Baekjoon/곱셈.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+long long ans;
+
+long long mul(long long a, long long b, long long c) {
+	if (b == 1) {
+		return a%c; 
+	}
+
+	long long val = mul(a, b / 2, c);
+	val = val * val %c;
+
+	//b- even
+	if (b % 2 == 0) {
+		return val;
+	}
+	//b- odd
+	return val*a%c; 
+}
+int main() {
+	cin >> a >> b >> c;
+	ans= mul(a, b, c);
+
+	cout << ans << '\n';
+	return 0; 
+}
