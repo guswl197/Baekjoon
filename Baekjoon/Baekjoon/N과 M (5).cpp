8e5f70f2a92cab3e7d9a9dcb14076edba3de454f
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-07-01 11:34:49 +0900

    백트래킹

---
+++ Baekjoon/Baekjoon/N과 M (5).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+int visit[10001]; 
+int arr[10]; 
+
+void backTracking(int cnt) {
+	if (cnt== m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n'; 
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			int x = v[i]; 
+			if (!visit[x]) {
+				visit[x] = 1; 
+				arr[cnt] = x; 
+				backTracking(cnt + 1); 
+				visit[x] = 0; 
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m; 
+	for (int i = 0; i < n; i++) {
+		int x; 
+		cin >> x;
+		v.push_back(x); 
+	}
+
+	sort(v.begin(), v.end()); 
+	
+	backTracking(0);
+
+	return 0; 
+}
