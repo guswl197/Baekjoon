1ca0a93c906aa81ceb8ee9f2fd47bcb671138ef0
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-07-28 14:00:33 +0900

    bfs

--- Baekjoon/Baekjoon/숨바꼭질 3.cpp
+++ Baekjoon/Baekjoon/숨바꼭질 3.cpp
@@ -2,42 +2,46 @@
 
 using namespace std;
 
-int n, k;
-int dist[200002];
+int n, k; 
+int visited[200002];
+
+int bfs() {
+	queue<int> q;
+	q.push(n);
+	visited[n] = 0;
+
+	while (!q.empty()) {
+		int cur = q.front(); 
+		q.pop(); 
+
+		if (cur == k) {
+			return visited[k]; 
+		}
+
+		if (2 * cur < 200002 && visited[2 * cur] == -1) {
+			visited[2 * cur] = visited[cur]; 
+			q.push(2 * cur); 
+		}
+
+		for (int next : {cur - 1, cur + 1}) {
+			if (next < 200002 && visited[next] == -1) {
+				visited[next] = visited[cur] + 1;
+				q.push(next); 
+			}
+		}
+	}
+
+	return 0; 
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> k;
-	int MAX = 200000;
-	fill(dist, dist + MAX, -1);
-	queue<int> dq;
-	dq.push(n);
-	dist[n] = 0;
-
-	while (!dq.empty()) {
-		int cur = dq.front();
-		dq.pop();
-
-		if (cur == k) {
-			break;
-		}
-
-		if (2 * cur < MAX && dist[2 * cur] == -1) {
-			dist[2 * cur] = dist[cur];
-			dq.push(2 * cur);
-		}
-
-		for (int nxt : {cur - 1, cur + 1}) {
-			if (nxt < 0 || nxt >= MAX || dist[nxt] != -1) {
-				continue;
-			}
-			dist[nxt] = dist[cur] + 1;
-			dq.push(nxt);
-		}
-	}
-
-	cout << dist[k];
-	return 0;
+	cin >> n >> k; 
+	fill(visited, visited + 200002, -1); 
+	
+	cout<<bfs()<<'\n';
+	
+	return 0; 
 }
