0b4aef2a152e208704e3aa215b7d469039ba16c9
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-07-29 11:39:21 +0900

    재귀

--- Baekjoon/Baekjoon/하노이 탑 이동 순서.cpp
+++ Baekjoon/Baekjoon/하노이 탑 이동 순서.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int n;
 
+void hanoi(int start, int end, int cnt) {
+	if (cnt ==1) {
+		cout << start << ' ' << end << '\n'; 
+		return; 
+	}
+
+	hanoi(start, 6 - start - end, cnt - 1); 
+	cout << start << ' ' << end << '\n'; 
+	hanoi(6 -start-end , end, cnt - 1); 
+}
+
 int main() {
 	cin >> n; 
+	int ans; 
+	cout << (1 << n) - 1 << '\n';
+	hanoi(1, 3, n); 
 
+	return 0; 
 }
