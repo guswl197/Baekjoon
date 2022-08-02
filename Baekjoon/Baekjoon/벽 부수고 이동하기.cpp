469912040fff9514ace0adf609af862f7a981911
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-02 15:02:40 +0900

    bfs

--- Baekjoon/Baekjoon/벽 부수고 이동하기.cpp
+++ Baekjoon/Baekjoon/벽 부수고 이동하기.cpp
@@ -3,55 +3,48 @@
 using namespace std;
 
 int n, m; 
-char board[1001][1001]; 
-int visited[1001][1001][2]; 
-int x, y, broken; 
-int dx[] = { 1,-1,0,0 }; 
-int dy[] = { 0,0,1,-1 }; 
+int x, y, b; 
+char arr[1001][1001]; 
+queue<tuple<int, int, int>> q; 
+int visited[1001][1001][2];
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1, 0,0 }; 
 
-int bfs() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			visited[i][j][0] = visited[i][j][1] = -1;
-		}
-	}
+int bfs(int si, int sj) {
+	q.push({ si,sj,0 }); 
+	visited[si][sj][0] = 1; 
 
-	visited[0][0][1] = visited[0][0][0] = 1; 
-
-	queue<tuple<int, int, int>> q; 
-	q.push({ 0,0,0 }); 
-	
 	while (!q.empty()) {
-		tie(x, y, broken) = q.front();
+		tie(x, y, b) = q.front(); 
 		q.pop();
-		
+
 		if (x == n - 1 && y == m - 1) {
-			return visited[x][y][broken];
+			return visited[x][y][b];
 		}
 
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i]; 
 			int ny = y + dy[i]; 
-			
+			int broken = b;
+
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
 				continue; 
 			}
 
-			if (board[nx][ny] == '0' && visited[nx][ny][broken] == -1) {
-				visited[nx][ny][broken] = visited[x][y][broken]+1; 
-				q.push({ nx,ny,broken }); 
+			if (!visited[nx][ny][broken] && arr[nx][ny]=='0') {
+				visited[nx][ny][broken] = visited[x][y][broken] + 1; 
+				q.push({ nx,ny, broken });
 			}
 
-			//벽 부수기
-			if (!broken && board[nx][ny] == '1' && visited[nx][ny][1] == -1) {
-				visited[nx][ny][1] = visited[x][y][broken]+1;
-				q.push({ nx,ny,1 });
+			if (!broken && !visited[nx][ny][1] && arr[nx][ny]=='1') {
+				visited[nx][ny][1] = visited[x][y][broken] + 1; 
+				q.push({ nx,ny,1}); 
 			}
-
 		}
+
 	}
 
-	return -1; 
+	return -1;
 }
 
 int main() {
@@ -60,10 +53,11 @@ int main() {
 	cin >> n >> m; 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> board[i][j]; 
+			cin >> arr[i][j]; 
 		}
 	}
 
-	cout<<bfs(); 
+	cout<<bfs(0,0); 
+
 	return 0;
 }
