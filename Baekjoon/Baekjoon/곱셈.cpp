11ed89e6617107f889d13cad9feb7f2b77390d6c
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-10 14:23:45 +0900

    재귀

--- Baekjoon/Baekjoon/곱셈.cpp
+++ Baekjoon/Baekjoon/곱셈.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long a, b, c;
-long long ans;
-
+long long a, b, c; 
+long long ans; 
+	
 long long mul(long long a, long long b, long long c) {
 	if (b == 1) {
-		return a % c;
+		return a % c; 
 	}
 
-	long long val = mul(a, b / 2, c);
-	val = val * val %c;
+	long long val = mul(a, b / 2, c); 
+	val = val * val %c; 
 
-	//b- even
+	//b : even
 	if (b % 2 == 0) {
 		return val;
 	}
-	//b- odd
-	return val * a%c;
-}
-int main() {
-	cin >> a >> b >> c;
-	ans = mul(a, b, c);
 
-	cout << ans << '\n';
-	return 0;
+	//b : odd
+	return val * a%c; 
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); 
+	cin >> a >> b >> c; 
+	ans = mul(a, b, c); 
+
+	cout << ans << '\n'; 
+	return; 
 }
