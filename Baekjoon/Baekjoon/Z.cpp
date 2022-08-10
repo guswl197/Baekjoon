3ded9f4ae21cad468216a47b6f006eee5f9ba1e4
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-10 17:19:20 +0900

    재귀

--- Baekjoon/Baekjoon/Z.cpp
+++ Baekjoon/Baekjoon/Z.cpp
@@ -1,20 +1,34 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int func(int n, int r, int c) {
-	if (n == 0) return 0;
+int n, r, c;
+int ans;
 
-	int half = 1 << (n - 1);
-	if (r < half && c < half) return func(n - 1, r, c);
-	if (r < half && c >= half) return half * half + func(n - 1, r, c - half);
-	if (r >= half && c < half) return 2 * half*half + func(n - 1, r - half, c);
-	return 3 * half*half + func(n - 1, r - half, c - half);
+int z(int n, int r, int c) {
+	if (n == 0) {
+		return 0; 
+	}
+	int div = 1 << (n - 1); 
+	if (r < div && c < div) {
+		return z(n - 1, r, c); 
+	}
+	if (r < div && c >= div) {
+		return div*div + z(n - 1, r, c-div);
+	}
+	if (r >= div && c < div) {
+		return div*div*2+ z(n - 1, r-div, c);
+	}
+	if (r >= div && c >= div) {
+		return div*div*3+ z(n - 1, r-div, c-div);
+	}
 }
 
-int main(void) {
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n, r, c;
 	cin >> n >> r >> c;
-	cout << func(n, r, c);
+	cout << z(n, r, c) << '\n';
+	return 0;
 }
+
--- Baekjoon/Baekjoon/하노이 탑 이동 순서.cpp
+++ Baekjoon/Baekjoon/하노이 탑 이동 순서.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int n;
 
 void hanoi(int start, int end, int cnt) {
-	if (cnt ==1) {
-		cout << start << ' ' << end << '\n'; 
-		return; 
+	if (cnt == 1) {
+		cout << start << ' ' << end << '\n';
+		return;
 	}
 
-	hanoi(start, 6 - start - end, cnt - 1); 
-	cout << start << ' ' << end << '\n'; 
-	hanoi(6 -start-end , end, cnt - 1); 
+	hanoi(start, 6 - start - end, cnt - 1);
+	cout << start << ' ' << end << '\n';
+	hanoi(6 - start - end, end, cnt - 1);
 }
 
 int main() {
-	cin >> n; 
-	int ans; 
+	cin >> n;
+	int ans;
 	cout << (1 << n) - 1 << '\n';
-	hanoi(1, 3, n); 
+	hanoi(1, 3, n);
 
-	return 0; 
+	return 0;
 }
