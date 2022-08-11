104b46e3e0b3883c9a7185c8be0b096df0faf477
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-11 12:04:26 +0900

    재귀

--- Baekjoon/Baekjoon/별 찍기 - 11.cpp
+++ Baekjoon/Baekjoon/별 찍기 - 11.cpp
@@ -2,47 +2,47 @@
 
 using namespace std;
 
-const int MAX = 1024 * 3 + 2; 
-int n; 
-char board[MAX][MAX * 2 - 1]; 
+const int MAX = 1024 * 3 + 2;
+int n;
+char board[MAX][MAX * 2 - 1];
 
-void fill_star(const int& x, const int& y) {
-	board[x][y] = '*'; 
+void fill_star(int x, int y) {
+	board[x][y] = '*';
 	board[x + 1][y - 1] = '*';
-	board[x + 1][y + 1] = '*'; 
+	board[x + 1][y + 1] = '*';
 
 	for (int i = y - 2; i <= y + 2; i++) {
-		board[x + 2][i] = '*'; 
+		board[x + 2][i] = '*';
 	}
 }
 
-void func(const int& s, const int& x, const int& y) {
+void func(int s, int x, int y) {
 	if (s == 3) {
-		fill_star(x, y); 
-		return; 
+		fill_star(x, y);
+		return;
 	}
-	int ns = s / 2; 
-	func(ns, x, y); 
-	func(ns, x + ns, y - ns); 
-	func(ns, x + ns, y + ns); 
+	int ns = s / 2;
+	func(ns, x, y);
+	func(ns, x + ns, y - ns);
+	func(ns, x + ns, y + ns);
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n; 
-	func(n, 0, n - 1); 
+	cin >> n;
+	func(n, 0, n - 1);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n * 2 - 1; j++) {
 			if (board[i][j] == '*') {
-				cout << '*'; 
+				cout << '*';
 			}
 			else {
-				cout << ' '; 
+				cout << ' ';
 			}
 		}
-		cout << '\n'; 
+		cout << '\n';
 	}
 
-	return 0; 
+	return 0;
 }
