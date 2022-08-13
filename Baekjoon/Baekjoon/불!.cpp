179469f1be613df8323d4f4fc4af621c6f8d0257
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-13 22:34:20 +0900

    bfs

--- Baekjoon/Baekjoon/불!.cpp
+++ Baekjoon/Baekjoon/불!.cpp
@@ -2,94 +2,109 @@
 
 using namespace std;
 
-int r, c; 
-char arr[1001][1001]; 
-int jvisited[1001][1001];
+int t; 
+int w, h; 
+char board[1001][1001]; 
 int fvisited[1001][1001]; 
-queue<pair<int, int>> jq;
-queue<pair<int, int>> fq; 
-int dx[] = { 1,-1 ,0,0 }; 
-int dy[] = { 0,0,1,-1 }; 
+int svisited[1001][1001]; 
+queue<pair<int, int>> fq;
+queue<pair<int, int>> sq;
+int dx[] = { 0,0,1,-1 }; 
+int dy[] = { 1,-1, 0,0 }; 
 
-int jbfs() {
-	while (!jq.empty()) {
-		int x = jq.front().first; 
-		int y = jq.front().second; 
-		jq.pop(); 
-
-		if (x == 0 || y == 0 || x == r - 1 || y == c - 1) {
-			return jvisited[x][y]+1;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i]; 
-			int ny = y + dy[i]; 
-
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-				continue; 
-			}
-			if (arr[nx][ny] == '.' &&!jvisited[nx][ny] && (fvisited[nx][ny] > jvisited[x][y] + 1 || !fvisited[nx][ny])) {
-				jvisited[nx][ny] = jvisited[x][y] + 1; 
-				jq.push({ nx,ny }); 
-			}
-		}
+void queueClear() {
+	while (!sq.empty()) {
+		sq.pop(); 
 	}
-
-	return 0; 
+	while (!fq.empty()) {
+		fq.pop();
+	}
 }
 
 void fbfs() {
 
 	while (!fq.empty()) {
 		int x = fq.front().first;
-		int y = fq.front().second;
-		fq.pop();
+		int y = fq.front().second;  
+		fq.pop(); 
 
 		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
+			int nx = x + dx[i]; 
+			int ny = y + dy[i]; 
+			if (nx < 0 || nx>= h || ny < 0 || ny >= w) {
 				continue;
 			}
-			if (arr[nx][ny] == '.' && !fvisited[nx][ny]) {
+			if (!fvisited[nx][ny] && board[nx][ny] != '#') {
 				fvisited[nx][ny] = fvisited[x][y] + 1;
-				fq.push({ nx,ny });
+				fq.push({ nx ,ny }); 
 			}
 		}
 	}
 }
 
+int sbfs() {
+
+	while (!sq.empty()) {
+		int x = sq.front().first;
+		int y = sq.front().second; 
+		sq.pop(); 
+
+		if (x == 0 || y == 0 || x == h - 1 || y == w - 1) {
+			return svisited[x][y];
+		}
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i]; 
+			int ny = y + dy[i]; 
+
+			if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
+				continue; 
+			}
+			if (svisited[nx][ny] || board[nx][ny] != '.') {
+				continue;
+			}
+			if (fvisited[nx][ny] ==0 || (fvisited[nx][ny] != 0 && svisited[x][y] + 1 < fvisited[nx][ny])) {
+				svisited[nx][ny] = svisited[x][y] + 1;
+				sq.push({ nx,ny }); 
+			}
+		}
+	}
+
+	return -1; 
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int ans=0; 
-	cin >> r >> c; 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			cin >> arr[i][j]; 
-		}
-	}
 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (arr[i][j] == 'F') {
-				fq.push({ i,j }); 
+	cin >> t;
+	while (t--) {
+		cin >> w >> h;
+		memset(board, 0, sizeof(board)); 
+		memset(fvisited, 0, sizeof(fvisited)); 
+		memset(svisited, 0, sizeof(svisited));
+		queueClear(); 
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				cin >> board[i][j];
+				if (board[i][j] == '*') {
+					fvisited[i][j] = 1; 
+					fq.push({ i,j });
+				}
+				else if (board[i][j] == '@') {
+					svisited[i][j] = 1;
+					sq.push({ i,j });
+				}
 			}
-			if (arr[i][j] == 'J') {
-				jq.push({ i,j });
-			}
 		}
+			fbfs(); 
+		
+			int ans = sbfs();
+			if (ans == -1) {
+				cout << "IMPOSSIBLE" << '\n';
+			}
+			else {
+				cout << ans << '\n'; 
+			}
 	}
-
-	fbfs(); 
-	ans=jbfs(); 
-
-	if (ans == 0) {
-		cout << "IMPOSSIBLE" << '\n';
-	}
-	else {
-		cout << ans << '\n';
-	}
-	return 0;
 }
