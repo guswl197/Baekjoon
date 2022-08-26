f2391bf594c4854674ee4dcd205e21e9fcadd3c7
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-26 15:42:16 +0900

    dp

--- Baekjoon/Baekjoon/2×n 타일링.cpp
+++ Baekjoon/Baekjoon/2×n 타일링.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int d[1001];
-int main(void) {
+int n; 
+int d[1001]; 
 
-	d[0] = 1;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n; 
+
 	d[1] = 1;
-	int n;
-	scanf("%d", &n);
+	d[2] = 2;
 
-	for (int i = 2; i <= n; i++) {
-		d[i] = d[i - 1] + d[i - 2];
-		d[i] %= 10007;
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2])%10007; 
 	}
-	printf("%d\n", d[n]);
-	return 0;
 
+	cout << d[n] << '\n'; 
+	return 0; 
 }
