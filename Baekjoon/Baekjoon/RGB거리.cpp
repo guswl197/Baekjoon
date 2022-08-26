4d686f67dc28e69d60914d2bbf0e2265a2926d3c
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-26 15:32:49 +0900

    dp

--- Baekjoon/Baekjoon/RGB거리.cpp
+++ Baekjoon/Baekjoon/RGB거리.cpp
@@ -1,32 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
-using namespace std; 
+using namespace std;
 
-int d[1005][5]; 
+int n;
+int arr[1001][5]; 
+int d[1001][5]; 
 
 int main() {
-	int n; 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	cin >> n; 
-	vector<vector<int >> v(n+2, vector<int>(5, 0)) ; 
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= 3; j++) {
-			cin >> v[i][j]; 
-		}
+		cin >> arr[i][1] >> arr[i][2] >> arr[i][3]; 
 	}
 
-	d[1][1] = v[1][1]; 
-	d[1][2] = v[1][2];
-	d[1][3] = v[1][3];
-	for (int i = 1; i <= n; i++) {
-		d[i][1] = min(d[i - 1][2], d[i - 1][3])+v[i][1];
-		d[i][2]= min(d[i - 1][1], d[i - 1][3]) + v[i][2];
-		d[i][3]= min(d[i - 1][1], d[i - 1][2]) + v[i][3];
-	}
-	
-	cout << min({ d[n][1], d[n][2], d[n][3] })<<endl;
+	d[1][1] = arr[1][1];
+	d[1][2] = arr[1][2];
+	d[1][3] = arr[1][3];
 
+	for (int i = 2; i <= n; i++) {
+		d[i][1] = min(d[i - 1][2], d[i - 1][3]) + arr[i][1];
+		d[i][2] = min(d[i - 1][1], d[i - 1][3]) + arr[i][2];
+		d[i][3] = min(d[i - 1][1], d[i - 1][2]) + arr[i][3];
+	}
+ 
+	cout << min({ d[n][1], d[n][2], d[n][3] }) << '\n'; 
 	return 0; 
 
 }
