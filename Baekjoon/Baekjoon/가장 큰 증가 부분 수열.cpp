be2c3b4d62ba95b074dedc042f9cabc39fe48bcc
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-08-29 11:32:22 +0900

    dp

--- Baekjoon/Baekjoon/가장 큰 증가 부분 수열.cpp
+++ Baekjoon/Baekjoon/가장 큰 증가 부분 수열.cpp
@@ -1,35 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int d[1005]; 
+int n; 
+vector<int> v; 
+int d[1001]; 
 
 int main() {
-	int n;
-	cin >> n;
-	vector<int> v(n+1); 
-	for (int i = 1; i <= n; i++) {
-		cin >> v[i]; 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n; 
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		v.push_back(x); 
 	}
 
-	d[1] = v[1]; 
-	for (int i = 2; i <= n; i++) {
-		d[i] = v[i];
-		for (int j = 1; j < i; j++) {
-			if (v[i] > v[j] && d[i] < d[j] + v[i]) {
-				d[i] = d[j] + v[i]; 
+	d[0] = v[0]; 
+	for (int i = 1; i < n; i++) {
+		int mx = 0; 
+		for (int j = 0; j < i; j++) {
+			if (v[i] > v[j]) {
+				mx = max(mx, d[j]); 
 			}
 		}
-
+		d[i] = max(v[i], mx + v[i]); 
 	}
 
 	int ans = 0; 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		ans = max(ans, d[i]); 
 	}
 
-	cout << ans << endl;
-	return 0; 
+	cout << ans << '\n'; 
+	return 0;
 }
