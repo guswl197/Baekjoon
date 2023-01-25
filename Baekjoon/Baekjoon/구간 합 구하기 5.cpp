5179d3243c322d6c2f536e6468d3b44e06551ebc
d이h <80441723+guswl197@users.noreply.github.com>
2023-01-25 11:00:21 +0900

    dp

--- Baekjoon/Baekjoon/구간 합 구하기 5.cpp
+++ Baekjoon/Baekjoon/구간 합 구하기 5.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m;
-int a[1030][1030], d[1030][1030];
+int n, m; 
+int x, y, w, z; 
+int arr[1025][1025];
+int d[1025][1025];
 
-int main(void) {
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	cin >> n >> m;
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= n; ++j) {
-			cin >> a[i][j];
-			d[i][j] = d[i - 1][j] + d[i][j - 1] - d[i - 1][j - 1] + a[i][j];
+	cin >> n >> m; 
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j]; 
+			d[i][j] = d[i][j - 1] + d[i - 1][j] - d[i - 1][j- 1] + arr[i][j]; 
 		}
 	}
 
-	while (m--) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		cout << d[x2][y2] - d[x1 - 1][y2] - d[x2][y1 - 1] + d[x1 - 1][y1 - 1] << '\n';
+	for (int i = 0; i < m; i++) {
+		cin >> w>> z >> x >> y;
+		cout << d[x][y] - d[x][z - 1] - d[w- 1][y] + d[w - 1][z- 1] << '\n'; 
 	}
+
+	return 0; 
 }
