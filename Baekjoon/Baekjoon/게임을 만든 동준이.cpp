0e39524b2b55eaef8c357fd80fec0688afaeb794
d이h <80441723+guswl197@users.noreply.github.com>
2023-01-31 19:26:18 +0900

    그리디

--- Baekjoon/Baekjoon/게임을 만든 동준이.cpp
+++ Baekjoon/Baekjoon/게임을 만든 동준이.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int n; 
-int arr[101]; 
+
+int n;
+int arr[101];
 int ans; 
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n; 
+	
+	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i]; 
 	}
 
-	for (int i = n - 2; i >= 0; i--) {
-		if (arr[i + 1] <= arr[i]) {
-			ans += arr[i] - arr[i + 1] + 1; 
-			arr[i] = arr[i + 1] - 1; 
+	for (int i = n - 1; i > 0; i--) {
+		if (arr[i] <= arr[i - 1]) {
+			ans += arr[i - 1] - arr[i] + 1;
+			arr[i - 1] = arr[i] - 1;
 		}
 	}
 
