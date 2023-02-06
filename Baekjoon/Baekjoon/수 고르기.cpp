5bb8492992b5e196eed203cf36f324a0cb647dd3
d이h <80441723+guswl197@users.noreply.github.com>
2023-02-06 13:50:44 +0900

    투 포인터

--- Baekjoon/Baekjoon/수 고르기.cpp
+++ Baekjoon/Baekjoon/수 고르기.cpp
@@ -2,32 +2,31 @@
 
 using namespace std;
 
-int n, m; 
-vector<int> v; 
-int mn= 2000000001;
+int n, m;
+int arr[100001]; 
+int mn = 0x7fffffff;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n >> m; 
-	for (int i = 0; i < n; i++) {
-		int x; 
-		cin >> x;
-		v.push_back(x); 
-	}
-	sort(v.begin(), v.end()); 
-	int idx=0; 
 
+	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
-		for (int j = idx; j < n; j++) {
-			if (abs(v[i] - v[j]) >= m) {
-				mn = min(mn, abs(v[i] - v[j]));
-				idx = j; 
-				break; 
-			}
+		cin >> arr[i]; 
+	}
+
+	sort(arr, arr + n); 
+	int en = 0;
+	for (int st = 0; st < n; st++) {
+		while (en < n && arr[en] - arr[st] < m) {
+			en++; 
 		}
+		if (en == n) {
+			break;
+		}
+		mn = min(mn, arr[en] - arr[st]); 
 	}
 
-	cout << mn << '\n'; 
+	cout << mn << '\n';
 	return 0; 
 }
