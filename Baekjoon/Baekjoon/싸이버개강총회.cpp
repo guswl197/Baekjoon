da021691a3fdc072862f36e2f81c33a6082cf320
d이h <80441723+guswl197@users.noreply.github.com>
2023-02-18 11:21:15 +0900

    unordered_set

--- Baekjoon/Baekjoon/싸이버개강총회.cpp
+++ Baekjoon/Baekjoon/싸이버개강총회.cpp
@@ -3,49 +3,45 @@
 
 using namespace std;
 
-int ttoi(string time) {
-	int t = 0; 
-	string h, m; 
+string S, E, Q; 
+string T, name; 
+unordered_set<string> in; 
+unordered_set<string> ans;
 
-	h = time.substr(0, 2); 
-	m = time.substr(3, 2); 
-	t = (60 * stoi(h)) + stoi(m);
-	return t; 
+int change(string str) {
+	int h, m; 
+	h = stoi(str.substr(0, 2));
+	m = stoi(str.substr(3,2));
+
+	return h * 60 + m; 
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	int cnt = 0; 
-	string	S, E, Q;
+
 	cin >> S >> E >> Q; 
-
-	int st_t, end_t, stream_t; 
-	st_t = ttoi(S); 
-	end_t = ttoi(E);
-	stream_t = ttoi(Q);
-
-	unordered_set<string> st_list;  // 시작 전에 입장한 학회원들
-	unordered_set<string> atd_list; // 최종 출석자 리스트 
+	int s= change(S);
+	int e = change(E); 
+	int q = change(Q);
 
 	while (1) {
-		string c_time, name; 
-		cin >> c_time >> name;
-		if (cin.eof() == true) {
-			break;
+		if (cin.eof()) {
+			break; 
+		}
+		
+		cin >> T >> name;
+		int t = change(T); 
+
+		if (t <= s) {
+			in.insert(name); 
+		}
+		else if (e <= t && t <= q && in.find(name) != in.end()) {
+			ans.insert(name); 
 		}
 
-		if (ttoi(c_time) <= st_t) {
-			st_list.insert(name); 
-		}
-		else if (ttoi(c_time) >= end_t && ttoi(c_time) <= stream_t) {
-			if (st_list.find(name) != st_list.end()) {
-				atd_list.insert(name); 
-			}
-		}
 	}
 
-	cout << atd_list.size() << '\n';
+	cout << ans.size() << '\n'; 
 	return 0; 
 }
