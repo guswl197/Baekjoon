7911ac81ed5c8c6d7883139ead216694622b3650
d이h <80441723+guswl197@users.noreply.github.com>
2023-03-07 10:34:36 +0900

    트리, bfs

--- Baekjoon/Baekjoon/트리의 부모 찾기.cpp
+++ Baekjoon/Baekjoon/트리의 부모 찾기.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-int n;
+int n; 
 vector<int> adj[100001]; 
-queue<int> q; 
-int parent[100001]; 
+int p[100001]; 
 
 void bfs(int x) {
+	queue<int> q; 
 	q.push(x); 
 
 	while (!q.empty()) {
-		int f = q.front(); 
+		int nx = q.front(); 
 		q.pop(); 
 
-		for (int i = 0; i < adj[f].size(); i++) {
-			if (parent[f] == adj[f][i]) {
-				continue;
+		for (auto ch : adj[nx]) {
+			if (p[nx] == ch) {
+				continue; 
 			}
-			q.push(adj[f][i]); 
-			parent[adj[f][i]] = f; 
+			q.push(ch); 
+			p[ch] = nx; 
 		}
 	}
 }
@@ -29,15 +29,15 @@ int main() {
 	cin.tie(0);
 	cin >> n; 
 	for (int i = 0; i < n-1; i++) {
-		int a, b;
+		int a, b; 
 		cin >> a >> b;
 		adj[a].push_back(b), adj[b].push_back(a); 
 	}
 
-	bfs(1);
+	bfs(1); 
 
 	for (int i = 2; i <= n; i++) {
-		cout << parent[i] << '\n'; 
+		cout << p[i] << '\n'; 
 	}
 
 	return 0; 
